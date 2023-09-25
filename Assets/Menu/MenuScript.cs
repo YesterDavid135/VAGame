68eabcb87ecb397ffa6e70c62a9941d797b7d1f4
yesterdavid135 <david.minder@sunrise.ch>
2023-09-25 07:48:17 +0200

    Added working Buttons

--- Assets/Menu/MenuScript.cs
+++ Assets/Menu/MenuScript.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Menu : MonoBehaviour
-{
-    // Called when we click the "Play" button.
-    public void OnPlayButton ()
-    {
-        SceneManager.LoadScene(1);
-    }
-    // Called when we click the "Quit" button.
-    public void OnQuitButton ()
-    {
-        Application.Quit();
+
+namespace Menu {
+    public class MenuScript : MonoBehaviour {
+        // Called when we click the "Play" button.
+        public void OnPlayButton() {
+            SceneManager.LoadScene(1);
+        }
+
+        // Called when we click the "Quit" button.
+        public void OnQuitButton() {
+            Application.Quit();
+        }
     }
 }
